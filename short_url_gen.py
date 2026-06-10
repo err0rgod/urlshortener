ca2eed3583b50e0e8d91c22f150173b2f3cf0b95
err0rgod <ndgaming458@gmail.com>
2026-06-11 00:01:35 +0530

    added timing

--- short_url_gen.py
+++ short_url_gen.py
@@ -53,20 +53,27 @@ def add_url():
 
 
 def serve_url(short_url : str):
+    start_time = time.perf_counter()
     cached = redis_client.get(short_url)
+    end_time = time.perf_counter()
 
     if cached:
         pass
-        # print(cached)
+        print(cached)
+        print(f"redis time : {(end_time-start_time)*1000}")
     else:   
+        start_time = time.perf_counter()
         long_url = get_long_url(short_url)
+        end_time = time.perf_counter()
         if long_url:
             redis_client.set(
                 short_url,
                 long_url,
                 ex = 3600
             )
+            print(f"DB time : {(end_time-start_time)*1000}")
             print(long_url)
+            
         else:
             print("URL does not exists")
 
@@ -74,17 +81,11 @@ def main():
     print(" 1. for Adding url 2. for fetching url")
     entry = int(input("Enter choice: "))
     if entry == 1:
-        start_time = time.perf_counter()
         add_url()
-        end_time = time.perf_counter()
 
     elif entry == 2:
         short_url = input("Enter short url: ")
-        start_time = time.perf_counter()
         serve_url(short_url)
-        end_time = time.perf_counter()
-    latency_ms = (end_time-start_time)*1000
-    print(f"operatioin tooke {latency_ms} ms")
 
 if __name__ == "__main__":
     main()
