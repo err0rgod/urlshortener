de3e1a9f70a34d2d855614da8d753f6b74edbb09
err0rgod <ndgaming458@gmail.com>
2026-06-10 23:56:01 +0530

    added cache layer

--- short_url_gen.py
+++ short_url_gen.py
@@ -4,6 +4,7 @@ from base62 import encode_base62
 from models import urldata
 from database import add_to_db, get_long_url, is_long_url_exists
 import redis
+import time
 
 redis_client = redis.Redis(
     host="localhost",
@@ -30,6 +31,7 @@ def get_unique_id() -> str:
 
 
 def add_url():
+    
     long_url = input("Enter the long url: ")
     short_url = get_short_url()
     url = urldata(
@@ -43,7 +45,7 @@ def add_url():
         pass
     else: 
         redis_client.set(
-            f"url:{short_url}",
+            short_url,
             long_url,
             ex=3600
             )
@@ -51,32 +53,38 @@ def add_url():
 
 
 def serve_url(short_url : str):
-    cached = redis_client.get(f"url:{short_url}")
+    cached = redis_client.get(short_url)
 
     if cached:
-        return cached
-    
-    long_url = get_long_url(short_url)
-
-    if long_url:
-        redis_client.set(
-            f"url:{short_url}",
-            long_url,
-            ex = 3600
-        )
-        print(long_url)
-
-    else:
-        print("URL does not exists")
+        pass
+        # print(cached)
+    else:   
+        long_url = get_long_url(short_url)
+        if long_url:
+            redis_client.set(
+                short_url,
+                long_url,
+                ex = 3600
+            )
+            print(long_url)
+        else:
+            print("URL does not exists")
 
 def main():
     print(" 1. for Adding url 2. for fetching url")
     entry = int(input("Enter choice: "))
     if entry == 1:
+        start_time = time.perf_counter()
         add_url()
+        end_time = time.perf_counter()
+
     elif entry == 2:
         short_url = input("Enter short url: ")
+        start_time = time.perf_counter()
         serve_url(short_url)
+        end_time = time.perf_counter()
+    latency_ms = (end_time-start_time)*1000
+    print(f"operatioin tooke {latency_ms} ms")
 
 if __name__ == "__main__":
     main()
