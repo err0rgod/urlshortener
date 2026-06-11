a7adc583b2028f796aa9d7f13ea8d9fdaa489d64
err0rgod <ndgaming458@gmail.com>
2026-06-11 22:12:32 +0530

    completed func

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from short_url_gen import get_short_url
+from short_url_gen import add_url, serve_url
 
 
 app = FastAPI()
@@ -7,5 +7,17 @@ app = FastAPI()
 
 @app.get("/")
 async def home_page(long_url : str | None = None):
-    short_url = get_short_url(long_url)
-    return short_url
+    if long_url:
+        short_url = add_url(long_url)
+        return short_url
+    else:
+        return "URL Shortener"
+    
+
+@app.get("/short/")
+async def give_short(short_url : str | None = None):
+    if short_url:
+        long_url = serve_url(short_url)
+        return long_url
+    else:
+        return "URL Shortener"
--- short_url_gen.py
+++ short_url_gen.py
@@ -30,9 +30,8 @@ def get_unique_id() -> str:
     return generator.next_id()
 
 
-def add_url():
+def add_url(long_url : str):
     
-    long_url = input("Enter the long url: ")
     short_url = get_short_url()
     url = urldata(
         short_url=short_url,
@@ -50,42 +49,43 @@ def add_url():
             ex=3600
             )
         add_to_db(url)
+    return short_url
 
 
 def serve_url(short_url : str):
-    start_time = time.perf_counter()
+    # start_time = time.perf_counter()
     cached = redis_client.get(short_url)
-    end_time = time.perf_counter()
+    # end_time = time.perf_counter()
 
     if cached:
         pass
-        print(cached)
-        print(f"redis time : {(end_time-start_time)*1000}")
+        return cached
+        # print(f"redis time : {(end_time-start_time)*1000}")
     else:   
-        start_time = time.perf_counter()
+        # start_time = time.perf_counter()
         long_url = get_long_url(short_url)
-        end_time = time.perf_counter()
+        # end_time = time.perf_counter()
         if long_url:
             redis_client.set(
                 short_url,
                 long_url,
                 ex = 3600
             )
-            print(f"DB time : {(end_time-start_time)*1000}")
-            print(long_url)
+            # print(f"DB time : {(end_time-start_time)*1000}")
+            return long_url
             
         else:
             print("URL does not exists")
 
-def main():
-    print(" 1. for Adding url 2. for fetching url")
-    entry = int(input("Enter choice: "))
-    if entry == 1:
-        add_url()
+# def main():
+#     print(" 1. for Adding url 2. for fetching url")
+#     entry = int(input("Enter choice: "))
+#     if entry == 1:
+#         add_url()
 
-    elif entry == 2:
-        short_url = input("Enter short url: ")
-        serve_url(short_url)
+#     elif entry == 2:
+#         short_url = input("Enter short url: ")
+#         serve_url(short_url)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
