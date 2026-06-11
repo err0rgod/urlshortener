c5bbdda360ecc19bdd2f79f61ec7c8317f8a8dbc
err0rgod <ndgaming458@gmail.com>
2026-06-11 23:23:19 +0530

    added routing paths

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse, HTMLResponse
 from short_url_gen import add_url, serve_url
 
 
 app = FastAPI()
 
 
-@app.get("/")
-async def home_page(long_url : str | None = None):
+@app.get("/",response_class=HTMLResponse)
+async def index():  
+    with open("templates/index.html") as f:
+        return f.read()
+
+
+@app.get("/{short_url}")
+async def get_short_give_long(short_url : str | None = None):
+    
+    long_url = serve_url(short_url)
+    return RedirectResponse(long_url,status_code=302)
+    
+@app.post(f"/")
+async def add_long_give_short(long_url : str | None = None):
     if long_url:
         short_url = add_url(long_url)
         return short_url
     else:
-        return "URL Shortener"
+        with open("templates/shortened.html") as f:
+            return f.read()
     
 
-@app.get("/short/")
-async def give_short(short_url : str | None = None):
-    if short_url:
-        long_url = serve_url(short_url)
-        return long_url
-    else:
-        return "URL Shortener"
